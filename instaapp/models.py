7842e4a21207ad45983a3b7581eab4fd10758e4e
ccbndh <tin.nguyen@alphabase.vn>
2015-10-06 00:32:11 +0700

    add model photo related with user

--- instaapp/models.py
+++ instaapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
+from image_cropping import ImageCropField, ImageRatioField
+from .helpers import *
 
-# Create your models here.
+
+class PhotoInstagram(models.Model):
+    user = models.ForeignKey(User, related_name="uploader")
+    title = models.CharField(max_length=120, blank=True, null=True)
+    photo = ImageCropField(upload_to=item_upload_to, blank=True)
+    cropping = ImageRatioField('photo', '300x300', size_warning=True, hide_image_field=True)
+    created = models.DateTimeField(auto_now_add=True)
--- instagram/settings.py
+++ instagram/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
     'instaapp',
+    'easy_thumbnails',
 )
 
 MIDDLEWARE_CLASSES = (
