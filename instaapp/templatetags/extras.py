6d0f35184eba57cbaa56f73689ed628fae52669b
ccbndh <tin.nguyen@alphabase.vn>
2015-10-07 21:40:29 +0700

    remove some debug

--- instaapp/templatetags/extras.py
+++ instaapp/templatetags/extras.py
@@ -9,7 +9,7 @@ def auto_add_link_hashtag(comment):
     res = ''
     for tag in comment.split():
         if tag.startswith("#"):
-            print(tag.strip("#"))
+            # print(tag.strip("#"))
             res += '<a href="http://127.0.0.1:8000/search/?search=' + tag.strip("#") + '">#' + tag.strip("#") + '</a>'
         else:
             res += tag
