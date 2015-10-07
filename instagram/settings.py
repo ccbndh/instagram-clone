b636b52c0081c8e84a4050b29db6e25c9f94501e
ccbndh <tin.nguyen@alphabase.vn>
2015-10-08 00:13:47 +0700

    change database mysql to sqlite for easy demo

--- instagram/settings.py
+++ instagram/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'instagram',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
